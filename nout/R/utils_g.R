9680336e0b876fcb6b7780b85f2fff225faa4318
chiaragaiamagnani <c.magnani9@campus.unimib.it>
2025-03-28 04:50:57 -0700

    Added standardization of the scores before estimating mixture Beta distribution in estimate_g function

--- nout/R/utils_g.R
+++ nout/R/utils_g.R
@@ -346,11 +346,13 @@ fit_beta_mixture <- function(data, num_starts = 10) {
 #' @return A list of three elements which are the estimated PDF and CDF and the monotonicity
 #' used in the estimation process.
 #'
-estimate_g <- function(scores_reference, scores_pooled, method="mixmodel", monotone=FALSE) {
+estimate_g <- function(scores_reference, scores_pooled, method="betamix", monotone=FALSE) {
     
-    # scores_std = standardize_to_uniform(scores_reference, scores_pooled)
-    # scores_reference = scores_std[[1]]
-    # scores_pooled = scores_std[[2]]
+    if(min(scores_reference) < 0 || max(scores_reference)>1 || min(scores_pooled) < 0 || max({scores_pooled)>1){
+      scores_std = standardize_to_uniform(scores_reference, scores_pooled)
+      scores_reference = scores_std[[1]]
+      scores_pooled = scores_std[[2]]
+    }
     
     ## Transform the reference scores to make them approximately uniform
     null.fit <- fitdistrplus::fitdist(as.numeric(scores_reference), "beta", start = list(shape1 = 0.999, shape2 = 0.999))
